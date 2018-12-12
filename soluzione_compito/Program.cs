923f055cf8791605009103c3151f8cac9eae6a85
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2018-12-12 08:59:40 +0100

    Ho chiesto in imput la base in cui voglio convertire il numero

--- soluzione_compito/Program.cs
+++ soluzione_compito/Program.cs
@@ -12,16 +12,27 @@ namespace soluzione_compito
         {
             string risultato="";
             int num;
+            int valBase;
             Console.WriteLine("Inserisci un numero");
             num = Convert.ToInt32(Console.ReadLine());
-            while(num>1)
+            do
             {
-                int resto = num % 8;
-                risultato = Convert.ToString(resto,8) + risultato;
-                num = num / 8;
+                Console.WriteLine("Inserisci la base in cui vuoi convertire il numero, quelle disponibili sono 2, 8 e 16");
+                valBase = Convert.ToInt32(Console.ReadLine());
+                if (valBase != 2 || valBase != 8 || valBase != 16)
+                {
+                    Console.WriteLine("ATTENZIONE, la base inserita non è disponinile");
+                }
+            } while (valBase != 2 || valBase != 8 || valBase != 16);
+
+            while (num>1)
+            {
+                int resto = num % valBase;
+                risultato = Convert.ToString(resto,valBase) + risultato;
+                num = num / valBase;
             }
             risultato = Convert.ToString(num) + risultato;
-            Console.WriteLine($"Il numero in base 8 è {risultato}");
+            Console.WriteLine($"Il numero in base {valBase} è {risultato}");
 
             Console.ReadLine();
 
