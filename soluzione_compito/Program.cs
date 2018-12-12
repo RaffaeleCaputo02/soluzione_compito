e4fd620b882bfbb14e50ca9be86da71e21083aee
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2018-12-12 08:52:28 +0100

    Ho convertito il numero inserito in base 8

--- soluzione_compito/Program.cs
+++ soluzione_compito/Program.cs
@@ -16,12 +16,12 @@ namespace soluzione_compito
             num = Convert.ToInt32(Console.ReadLine());
             while(num>1)
             {
-                int resto = num % 16;
-                risultato = Convert.ToString(resto,16) + risultato;
-                num = num / 16;
+                int resto = num % 8;
+                risultato = Convert.ToString(resto,8) + risultato;
+                num = num / 8;
             }
             risultato = Convert.ToString(num) + risultato;
-            Console.WriteLine($"Il numero in base 16 è {risultato}");
+            Console.WriteLine($"Il numero in base 8 è {risultato}");
 
             Console.ReadLine();
 
