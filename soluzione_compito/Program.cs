85e38d3609c00f30c2a9bc61a898d7c38c39e68f
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2018-12-12 08:32:30 +0100

    soluzione vuota

---
+++ soluzione_compito/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace soluzione_compito
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
